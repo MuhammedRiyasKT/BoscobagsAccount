97d4d0ce70ad850b0224e0db619ea4436f3687a5
MuhammedRiyasKT <mhdriyaskt10@gmail.com>
2026-04-07 00:54:02 +0530

    backend link updated

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://boscobags-account-server.onrender.com/api',
 });
 
 // Interceptor to add token to requests
